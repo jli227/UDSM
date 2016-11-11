313006ed846967e0c4083c9af50b1fafcff7249b
Jonathan Li <jli227@uw.edu>
2016-11-11 01:11:46 -0800

    tweaking heroku configuration

--- config/config.donate.js
+++ config/config.donate.js
@@ -1,25 +1,34 @@
 module.exports = {
-  "title": "Donate",
-  "sections": [
-    {
-      "header": "MONETARY DONATIONS",
-      "body": "UDSM provides health care to the U-District population experiencing homelessness" +
-      "By helping UDSM, you decrease the burden on the underserved by keeping our services " +
-      "free to our patients",
-      "link": {"title": "DONATE TODAY", "link": "http://depts.washington.edu/givemed/give/?page=make&source=servmd"}
-    },
-    {
-    	"lists" : [
-    	{"header": "MEDICAL", "list": ["Drapes", "BP cuffs (small, regular, long, large)", "Gowns", "Stethoscopes", "Thermometeres", "Yoga mats", "Alcohol wipes",
-    	"\"Purple\" sanitizing wipes", "Massage tables", "Wound care/bandage supplies", "Plastic soaking bin", "Gloves", "Hand sanitizer",
-    	"Patient bibs", "Flashlights", "Disposable mirrors", "Flouride", "Pulse oximeter", "Temp machine", "Sterile toenail clippers",
-    	"Otoscope tips", "Printing costs"]}, 
-    	{"header": "OTHER", "list": ["Wound care/first aid supplies", "Warm socks", "Bottled water", "Hats/gloves", "Hand warmers",
-    	"Coffee gift cards", "Dollar tree gift cards", "Safeway gift cards", "Tents", "Sleeping bags", "Large black garbage bags",
-    	"Prepaid phone cards"]}
-    	],
-    	"footer" : "Donations can be dropped off at the Christ Episcopal Church",
-    	"footerAddress" : "4548 Brooklyn Ave NE Seattle, WA 98105"
-    }
-  ]
+    "title": "Donate",
+    "sections": [
+        {
+            "header": "MONETARY DONATIONS",
+            "body": "UDSM provides health care to the U-District population experiencing homelessness" +
+            "By helping UDSM, you decrease the burden on the underserved by keeping our services " +
+            "free to our patients",
+            "link": {
+                "title": "DONATE TODAY",
+                "link": "http://depts.washington.edu/givemed/give/?page=make&source=servmd"
+            }
+        },
+        {
+            "lists": [
+                {
+                    "header": "MEDICAL",
+                    "list": ["Drapes", "BP cuffs (small, regular, long, large)", "Gowns", "Stethoscopes", "Thermometeres", "Yoga mats", "Alcohol wipes",
+                        "\"Purple\" sanitizing wipes", "Massage tables", "Wound care/bandage supplies", "Plastic soaking bin", "Gloves", "Hand sanitizer",
+                        "Patient bibs", "Flashlights", "Disposable mirrors", "Flouride", "Pulse oximeter", "Temp machine", "Sterile toenail clippers",
+                        "Otoscope tips", "Printing costs"]
+                },
+                {
+                    "header": "OTHER",
+                    "list": ["Wound care/first aid supplies", "Warm socks", "Bottled water", "Hats/gloves", "Hand warmers",
+                        "Coffee gift cards", "Dollar tree gift cards", "Safeway gift cards", "Tents", "Sleeping bags", "Large black garbage bags",
+                        "Prepaid phone cards"]
+                }
+            ],
+            "footer": "Donations can be dropped off at the Christ Episcopal Church",
+            "footerAddress": "4548 Brooklyn Ave NE Seattle, WA 98105"
+        }
+    ]
 };
